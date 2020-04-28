080bff4a2565d799fa786a3efc4359aeaae00c65
varte <varte@eng.ucsd.edu>
2020-04-28 00:07:08 -0700

    added query limit for INT-CTXT game

--- games/game_int_ctxt.py
+++ games/game_int_ctxt.py
@@ -9,14 +9,14 @@ class GameINTCTXT(Game):
     have been sent by the sender. The Adversary has access to an encryption
     oracle (enc) and a decryption oracle (dec) that it uses to see if it won.
     """
-    def __init__(self, encrypt, decrypt, key_len):
+    def __init__(self, required_queries, encrypt, decrypt, key_len):
         """
         :param encrypt: Encryption function that takes inputs, a key k of
                         key_len length and a message.
         :param decrypt: Decryption function to match encryption function.
         :param key_len: Length of key used by encrypt and decrypt.
         """
-        self._enc, self._dec, self.key_len = encrypt, decrypt, key_len
+        self.required_queries, self._enc, self._dec, self.key_len = required_queries, encrypt, decrypt, key_len
         self.key = ''
         self.ciphertexts = []
 
@@ -25,6 +25,7 @@ class GameINTCTXT(Game):
         Initializes key to be used in encryption. Called by simulator to
         reset game state in between runs.
         """
+        self.answered_queries = 0
         self.key = random_string(self.key_len)
         self.ciphertexts = []
         self.win = False
@@ -36,6 +37,7 @@ class GameINTCTXT(Game):
         :param m: Message to be encrypted.
         :return: Cipher text if valid, ``None`` otherwise.
         """
+        self.answered_queries += 1
         c = self._enc(self.key, m)
         self.ciphertexts += [c]
         return c
@@ -46,6 +48,8 @@ class GameINTCTXT(Game):
 
         :return: True if win, False otherwise.
         """
+        if self.required_queries < self.answered_queries:
+            raise ValueError("The adversary made " + str(self.answered_queries) + " queries to its enc oracle. It is required to make at most " + str(self.required_queries) + " call(s) to enc.")
         m = self._dec(self.key, c)
         if c not in self.ciphertexts and m is not None:
             return True
