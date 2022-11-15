9c1a38b9b9df26470d41a77ad4e15f5f731c1243
yzyzb <z2yuan@ucsd.edu>
2022-11-15 00:25:02 -0800

    updated the vol function family to new_tools

--- src/playcrypt/new_tools.py
+++ src/playcrypt/new_tools.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join('..')))
+from Crypto.Hash import SHA256
 from playcrypt.tools import *    
     
 def MOD_INV(a,N):
@@ -28,3 +29,25 @@ def K_rsa(k):
 def random_string_as_integer(length):
     r = random.randint(0, 2**(length)-1)
     return r
+
+def VOL(j):
+    def vol(x, l):
+        """
+        The variable output length function
+        :param x: an int type that represents the input string to hash
+        :param l: output length
+        :return:  a binary string 
+        """
+        n = -(l // -256)
+        Y = ''
+        h = SHA256.new()
+        l = l % (2**64)
+        h.update(bytes(int_to_string(j,8) + int_to_string(l,8) + int_to_string(x), 'utf-8'))
+        P = h.hexdigest()
+        for i in range(n):
+            i = i % (2**64)
+            h.update(bytes(int_to_string(i,8) + P, 'utf-8'))
+            Y += h.hexdigest()
+        Y = bin(string_to_int(Y))
+        return Y[2: l+2]
+    return vol
